4cc50545d94585a090ce874c7aaecf198e9219d8
Edythe Palmer <palmer.edythe@gmail.com>
2025-04-12 17:20:27 -0400

    Added camera rotation

--- camera_3d.gd
+++ camera_3d.gd
@@ -1,3 +1,6 @@
 extends Camera3D
 func _physics_process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed("camera_left"):
+		rotation.y += 0.01
+	if Input.is_action_pressed("camera_right"):
+		rotation.y -= 0.01
