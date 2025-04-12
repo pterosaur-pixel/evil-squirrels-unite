4922c9f5a733d7b0c3cf2112b7f5190a5d7cfe2d
Edythe Palmer <palmer.edythe@gmail.com>
2025-04-12 16:12:07 -0400

    Made water slow down character
    Also changed gravity to 3 rather than 5
    Speed variable in character

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,12 +1,20 @@
 extends CharacterBody3D
+var speed = 30
 func _physics_process(_delta: float) -> void:
 	if not is_on_floor():
-		velocity.y -= 5
+		velocity.y -= 3
 	var direction := Input.get_axis("ui_left", "ui_right")
 	var direction_z := Input.get_axis("ui_up", "ui_down")
-	velocity.z = direction_z * 30
-	velocity.x = direction * 30
+	velocity.z = direction_z * speed
+	velocity.x = direction * speed
 	if Input.is_action_just_pressed("jump"):
-		velocity.y += 50
-		
+		velocity.y = 50
 	move_and_slide()
+
+
+func _on_water_collider_area_entered(area: Area3D) -> void:
+	speed = 15
+
+
+func _on_water_collider_area_exited(area: Area3D) -> void:
+	speed = 30
