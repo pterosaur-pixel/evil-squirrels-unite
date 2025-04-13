a8d04dc5115a581184d425d9bf9d122d585a62f9
Edythe Palmer <palmer.edythe@gmail.com>
2025-04-13 11:55:36 -0400

    Made camera move with character's y position

--- camera_3d.gd
+++ camera_3d.gd
@@ -3,6 +3,7 @@ extends Camera3D
 func _physics_process(delta: float) -> void:
 	global_position.x = character.global_position.x + 2
 	global_position.z = character.global_position.z + 5
+	global_position.y = character.global_position.y + 2
 	if Input.is_action_pressed("camera_left"):
 		rotation.y += 0.01
 	if Input.is_action_pressed("camera_right"):
