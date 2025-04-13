1727c89150aa20a2a96df1297b15d051c4ac7b04
Edythe Palmer <palmer.edythe@gmail.com>
2025-04-13 12:04:59 -0400

    Made character turn when he changes direction
    Took out camera tilt

--- camera_3d.gd
+++ camera_3d.gd
@@ -1,7 +1,7 @@
 extends Camera3D
 @onready var character = get_node("../GoobleSnoop")
 func _physics_process(delta: float) -> void:
-	global_position.x = character.global_position.x + 2
+	global_position.x = character.global_position.x + 1
 	global_position.z = character.global_position.z + 5
 	global_position.y = character.global_position.y + 2
 	if Input.is_action_pressed("camera_left"):
--- character_body_3d.gd
+++ character_body_3d.gd
@@ -7,10 +7,17 @@ func _physics_process(_delta: float) -> void:
 	var direction_z := Input.get_axis("ui_up", "ui_down")
 	velocity.z = direction_z * speed
 	velocity.x = direction * speed
+	if direction != 0:
+		rotation.y = -direction * PI/2
+	if direction_z != 0:
+		if direction_z == -1:
+			rotation.y = 0
+		else:
+			rotation.y = PI
 	if Input.is_action_just_pressed("jump"):
 		velocity.y = 50
 	move_and_slide()
-	print(global_position)
+	print(rotation)
 func _on_water_collider_area_entered(area: Area3D) -> void:
 	speed = 15
 
