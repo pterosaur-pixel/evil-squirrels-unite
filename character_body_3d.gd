889d3a2fe3fa490f25de3504c5848c0b1e651936
Edythe Palmer <palmer.edythe@gmail.com>
2025-04-13 12:32:38 -0400

    Gave character moving legs

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 var speed = 30
+var leg_1_direction = 0.15
+var leg_2_direction = -0.15
 func _physics_process(_delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= 3
@@ -16,8 +18,26 @@ func _physics_process(_delta: float) -> void:
 			rotation.y = PI
 	if Input.is_action_just_pressed("jump"):
 		velocity.y = 50
-	move_and_slide()
-	print(rotation)
+	if velocity.y != 0:
+		$Leg1.rotation.x = -PI/2.5
+		$Leg2.rotation.x = -PI/2.5
+	
+	elif direction != 0 or direction_z != 0:
+		print('moving')
+		if $Leg1.rotation.x > PI/2.9 or $Leg1.rotation.x < -PI/2.5:
+			leg_1_direction = -leg_1_direction
+		leg_2_direction = -leg_1_direction
+		#if $Leg2.rotation.x > PI/2.9 or $Leg2.rotation.x < -PI/2.5:
+			#leg_2_direction = -leg_2_direction
+		$Leg1.rotation.x += leg_1_direction
+		$Leg2.rotation.x += leg_2_direction
+	else:
+		$Leg1.rotation.x = 0
+		$Leg2.rotation.x = 0	
+	
+	
+	
+	move_and_slide()	
 func _on_water_collider_area_entered(area: Area3D) -> void:
 	speed = 15
 
